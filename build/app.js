6ae2b04580af212f8135a5e8ea51158855f8c24b
Loek van der Linde <lind0096@hz.nl>
2019-11-05 12:54:47 +0100

    Compile code

--- build/app.js
+++ build/app.js
@@ -1,22 +1,40 @@
-class Animal {
-    constructor(name, legs, sound) {
-        this.name = name;
-        this.legs = legs;
-        this.sound = sound;
+class Game {
+    constructor(canvasId) {
+        this.player = "Player1";
+        this.score = 400;
+        this.lives = 3;
+        this.canvas = canvasId;
+        this.canvas.width = window.innerWidth;
+        this.canvas.height = window.innerHeight;
+        this.ctx = this.canvas.getContext('2d');
+        this.highscores = [
+            {
+                playerName: 'Loek',
+                score: 40000
+            },
+            {
+                playerName: 'Daan',
+                score: 34000
+            },
+            {
+                playerName: 'Rimmert',
+                score: 200
+            }
+        ];
+        this.start_screen();
     }
-    getName() {
-        return this.name;
+    start_screen() {
     }
-    getLegs() {
-        return this.legs;
+    level_screen() {
     }
-    getSound() {
-        return this.sound;
+    title_screen() {
+    }
+    randomNumber(min, max) {
+        return Math.round(Math.random() * (max - min) + min);
     }
 }
-const animals = [
-    new Animal('dog', 4, 'woof'),
-    new Animal('cat', 4, 'meow')
-];
-animals.forEach((animal) => console.log('A %s has %s legs and goes %s!', animal.getName(), animal.getLegs(), animal.getSound()));
+let init = function () {
+    const Asteroids = new Game(document.getElementById('canvas'));
+};
+window.addEventListener('load', init);
 //# sourceMappingURL=app.js.map
