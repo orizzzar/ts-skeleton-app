f486bf516da3c9627847ec4c113fbc528dc2f7d8
Loek van der Linde <lind0096@hz.nl>
2019-11-11 17:33:16 +0100

    TSLint the hell out of our code

--- src/Game.ts
+++ src/Game.ts
@@ -1,14 +1,16 @@
+// tslint:disable member-ordering
+
 class Game {
     // Global attributes for canvas
     // Readonly attributes are read-only. They can only be initialized in the constructor
-    private readonly canvas: HTMLCanvasElement; 
+    private readonly canvas: HTMLCanvasElement;
     private readonly ctx: CanvasRenderingContext2D;
 
     // Some global player attributes
     private readonly player: string;
     private readonly score: number;
     private readonly lives: number;
-    private readonly highscores: Array<any>; // TODO: do not use 'any': write an interface!
+    private readonly highscores: object[]; // TODO: do not use 'any': write an interface!
 
     public constructor(canvasId: HTMLCanvasElement) {
         // Construct all of the canvas
@@ -16,7 +18,7 @@ class Game {
         this.canvas.width = window.innerWidth;
         this.canvas.height = window.innerHeight;
         // Set the context of the canvas
-        this.ctx = this.canvas.getContext('2d');
+        this.ctx = this.canvas.getContext("2d");
 
         this.player = "Player one";
         this.score = 400;
@@ -24,18 +26,18 @@ class Game {
 
         this.highscores = [
             {
-                playerName: 'Loek',
-                score: 40000
+                playerName: "Loek",
+                score: 40000,
             },
             {
-                playerName: 'Daan',
-                score: 34000
+                playerName: "Daan",
+                score: 34000,
             },
             {
-                playerName: 'Rimmert',
-                score: 200
-            }
-        ]
+                playerName: "Rimmert",
+                score: 200,
+            },
+        ];
 
         // All screens: uncomment to activate
         this.startScreen();
@@ -44,28 +46,28 @@ class Game {
 
     }
 
-    //-------- Splash screen methods ------------------------------------
+    // -------- Splash screen methods ------------------------------------
     /**
      * Method to initialize the splash screen
      */
     public startScreen() {
-        //1. add 'Asteroids' text
+        // 1. add 'Asteroids' text
         this.writeTextToCanvas("Asteroids", 140, this.canvas.width / 2, 150);
 
-        //2. add 'Press to play' text
-        this.writeTextToCanvas("PRESS PLAY TO START", 40, this.canvas.width / 2, this.canvas.height / 2 -20)
+        // 2. add 'Press to play' text
+        this.writeTextToCanvas("PRESS PLAY TO START", 40, this.canvas.width / 2, this.canvas.height / 2 - 20);
 
-        //3. add button with 'start' text
+        // 3. add button with 'start' text
         const asteroidFileName = "./assets/images/SpaceShooterRedux/PNG/UI/buttonBlue.png";
         this.loadImage(asteroidFileName, this.writeStartButtonToStartScreen);
-        //4. add Asteroid image
+        // 4. add Asteroid image
         const startButtonFileName = "./assets/images/SpaceShooterRedux/PNG/Meteors/meteorBrown_big1.png";
         this.loadImage(startButtonFileName, this.writeAsteroidImageToStartScreen);
     }
 
     /**
      * Writes the loaded asteroids image pixels to the start screen
-     * 
+     *
      * @param {HTMLImageElement} img the loaded image object
      */
     private writeAsteroidImageToStartScreen(img: HTMLImageElement) {
@@ -78,7 +80,7 @@ class Game {
 
     /**
      * Writes the loaded start button image to the start screen and writes a text on top of it
-     * 
+     *
      * @param {HTMLImageElement} img the loaded image object
      */
     private writeStartButtonToStartScreen(img: HTMLImageElement) {
@@ -87,23 +89,23 @@ class Game {
 
         this.ctx.drawImage(img, x  - img.width / 2, y);
 
-        this.writeTextToCanvas("Play", 20, x, y + 26, 'center', 'black');
+        this.writeTextToCanvas("Play", 20, x, y + 26, "center", "black");
     }
 
-    //-------- level screen methods -------------------------------------
+    // -------- level screen methods -------------------------------------
     /**
      * Method to initialize the level screen
      */
     public levelScreen() {
-        //1. load life images
+        // 1. load life images
         const lifeImageFileName = "./assets/images/SpaceShooterRedux/PNG/UI/playerLife1_blue.png";
         this.loadImage(lifeImageFileName, this.writeLifeImagesToLevelScreen);
 
-        //2. draw current score
-        this.writeTextToCanvas(`Your score: ${this.score}`, 20, this.canvas.width - 100, 30, 'right');
+        // 2. draw current score
+        this.writeTextToCanvas(`Your score: ${this.score}`, 20, this.canvas.width - 100, 30, "right");
 
-        //3. draw random asteroids
-        const asteroids: Array<string> = [
+        // 3. draw random asteroids
+        const asteroids: string[] = [
             "./assets/images/SpaceShooterRedux/PNG/Meteors/meteorBrown_big1.png",
             "./assets/images/SpaceShooterRedux/PNG/Meteors/meteorBrown_big2.png",
             "./assets/images/SpaceShooterRedux/PNG/Meteors/meteorBrown_big3.png",
@@ -124,33 +126,33 @@ class Game {
             this.loadImage(asteroids[index], this.writeAsteroidImageToRandomLocationOnLevelScreen);
         }
 
-        //4. draw player spaceship
+        // 4. draw player spaceship
         const playerSpaceShipFileName = "./assets/images/SpaceShooterRedux/PNG/playerShip1_blue.png";
         this.loadImage(playerSpaceShipFileName, this.writePlayerShipToLevelScreen);
     }
 
     /**
-     * Uses the loaded life image to remaining lives of the player on the rop 
+     * Uses the loaded life image to remaining lives of the player on the rop
      * left of the screen.
-     * 
+     *
      * @param {HTMLImageElement} img the loaded image object
      */
     private writeLifeImagesToLevelScreen(img: HTMLImageElement) {
         let x = 10;
-        const y = img.height -10;
+        const y = img.height - 10;
         // Start a loop for each life in lives
-        for (let life=0; life<this.lives; life++) {
+        for (let life = 0; life < this.lives; life++) {
             // Draw the image at the curren x and y coordinates
             this.ctx.drawImage(img, x, y);
             // Increase the x-coordinate for the next image to draw
-            x += img.width + 10; 
+            x += img.width + 10;
         }
     }
 
 
     /**
      * Writes a loaded asteroid image at a random location on the screen.
-     * 
+     *
      * @param {HTMLImageElement} img the loaded image object
      */
     private writeAsteroidImageToRandomLocationOnLevelScreen(img: HTMLImageElement) {
@@ -161,7 +163,7 @@ class Game {
 
     /**
      * Writes the loaded Player Ship image to the center of the screen
-     * 
+     *
      * @param {HTMLImageElement} img the loaded image object
      */
     private writePlayerShipToLevelScreen(img: HTMLImageElement) {
@@ -171,29 +173,29 @@ class Game {
         this.ctx.drawImage(img, x, y);
     }
 
-    //-------- Title screen methods -------------------------------------
+    // -------- Title screen methods -------------------------------------
 
     /**
-    * Method to initialize the title screen
-    */
+     * Method to initialize the title screen
+     */
     public titleScreen() {
         const x = this.canvas.width / 2;
         let y = this.canvas.height / 2;
 
-        //1. draw your score
+        // 1. draw your score
         this.writeTextToCanvas(`${this.player} score is ${this.score}`, 80, x, y - 100);
 
-        //2. draw all highscores
+        // 2. draw all highscores
         this.writeTextToCanvas("HIGHSCORES", 40, x, y);
 
-        for (let i=0; i<this.highscores.length; i++) {
+        for (let i = 0; i < this.highscores.length; i++) {
             y += 40;
             const text = `${i + 1}: ${this.highscores[i].playerName} - ${this.highscores[i].score}`;
             this.writeTextToCanvas(text, 20, x, y);
         }
     }
 
-    //-------Generic canvas methods ----------------------------------
+    // -------Generic canvas methods ----------------------------------
 
     /**
      * Writes text to the canvas
@@ -210,7 +212,7 @@ class Game {
         xCoordinate: number,
         yCoordinate: number,
         alignment: CanvasTextAlign = "center",
-        color: string = "white"
+        color: string = "white",
     ) {
         this.ctx.font = `${fontSize}px Minecraft`;
         this.ctx.fillStyle = color;
@@ -241,7 +243,7 @@ class Game {
      * @param {Function} callback - method that is invoked after the image is loaded
      */
     private loadImage(source: string, callback: Function) {
-        let imageElement = new Image();
+        const imageElement = new Image();
 
         // We must wait until the image file is loaded into the element
         // We add an event listener
@@ -255,19 +257,19 @@ class Game {
     }
 
     /**
-    * Renders a random number between min and max
-    * @param {number} min - minimal time
-    * @param {number} max - maximal time
-    */
+     * Renders a random number between min and max
+     * @param {number} min - minimal time
+     * @param {number} max - maximal time
+     */
     public randomNumber(min: number, max: number): number {
         return Math.round(Math.random() * (max - min) + min);
     }
 }
 
-//this will get an HTML element. I cast this element in de appropriate type using <>
-let init = function () {
-    const Asteroids = new Game(<HTMLCanvasElement>document.getElementById('canvas'));
+// This will get an HTML element. I cast this element in de appropriate type using <>
+let init = function() {
+    const Asteroids = new Game(document.getElementById("canvas") as HTMLCanvasElement);
 };
 
 // Add EventListener to load the game whenever the browser is ready
-window.addEventListener('load', init);
+window.addEventListener("load", init);
